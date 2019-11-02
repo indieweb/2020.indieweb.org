a52ddb6b281c2c5217c972894a0604ae48706c7d
Aaron Parecki <aaron@parecki.com>
2019-11-02 11:32:56 -0500

    add index and webmention pages

---
+++ public/index.php
@@ -0,0 +1,121 @@
+<?php
+$title = 'IndieWeb Events';
+$year = 2020;
+$url = 'https://2020.indieweb.org/';
+$summary = 'IndieWeb upcoming events.';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+
+  <title><?= $title ?> <?= $year ?></title>
+
+  <link rel="webmention" href="/webmention.php">
+
+  <link rel="stylesheet" type="text/css" href="/semantic/semantic.min.css">
+  <link rel="stylesheet" type="text/css" href="/assets/icomoon/style.css">
+  <link rel="stylesheet" type="text/css" href="/assets/styles.css?2">
+  <link rel="stylesheet" href="/assets/leaflet/leaflet.css" />
+  <script src="/assets/jquery-2.2.0.min.js"></script>
+  <script src="/semantic/semantic.min.js"></script>
+  <script src="/assets/leaflet/leaflet.js"></script>
+  <script src='https://js.tito.io/v1' async></script>
+
+  <meta property="og:url" content="<?= $url ?>">
+  <meta property="og:type" content="website">
+  <meta property="og:title" content="<?= $title ?> - <?= $year ?>">
+  <meta property="og:description" content="<?= htmlspecialchars($summary) ?>">
+</head>
+<body>
+
+  <div class="ui vertical stripe segment orange-bkg">
+    <div class="ui text container">
+      <div style="font-size: 2em; text-align: center; color: white" class="carrot-white">
+        IndieWebCamps in <?= $year ?>
+      </div>
+    </div>
+  </div>
+
+
+<!-- Page Contents -->
+<div class="pusher">
+  <div class="ui vertical stripe segment h-feed" id="events">
+    <div class="ui text container">
+
+
+
+
+      <div class="h-event vevent">
+        <h3 class="ui header p-name summary">
+          <a class="u-url url" href="https://2020.indieweb.org/austin">IndieWebCamp Austin</a>
+        </h3>
+        <p>
+          <span class="p-location location">Austin, Texas</span><br />
+          <time class="dt-start dtstart" datetime="2020-02-22">February 22</time>-<time class="dt-end dtend" datetime="2020-02-23">23, 2020</time>
+        </p>
+      </div>
+
+
+
+      <div class="h-event vevent">
+        <h3 class="ui header p-name summary">
+          <a class="u-url url" href="https://2020.indieweb.org/summit">IndieWeb Summit</a>
+        </h3>
+        <p>
+          <span class="p-location location">Portland, Oregon</span><br />
+          <time class="dt-start dtstart" datetime="2020-06-26">June 26</time>-<time class="dt-end dtend" datetime="2020-06-27">27, 2020</time>
+        </p>
+      </div>
+
+
+
+
+      <!--
+      <h2 style="margin-top:6em;">Past Events</h2>
+      -->
+
+
+
+
+
+
+
+
+
+      <div style="margin-top: 100px;">
+        <p>See more related events at <a href="https://indieweb.org/events">indieweb.org/events</a></p>
+      </div>
+
+    </div>
+  </div>
+
+  <div class="ui vertical stripe segment orange-bkg" id="social-media-section">
+    <div class="ui text container">
+      <div style="font-size: 2em; text-align: center;" class="carrot-white">
+        #indiewebcamp
+      </div>
+      <div style="font-size: 4em; text-align: center;" class="social-media-icons">
+        <a href="https://indieweb.org"><i class="ui attach icon"></i></a>
+        <a href="https://twitter.com/indiewebcamp"><i class="ui twitter icon"></i></a>
+        <a href="https://www.facebook.com/indiewebcamp/"><i class="ui facebook icon"></i></a>
+      </div>
+    </div>
+  </div>
+
+</div>
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-16359758-38', 'auto');
+  ga('send', 'pageview');
+
+</script>
+</body>
+</html>
---
+++ public/webmention.php
@@ -0,0 +1,138 @@
+<?php
+chdir(__DIR__);
+require_once('../lib/HTTP.php');
+require_once('../lib/config.php');
+require_once('../lib/rsvps.php');
+
+$http = new HTTP();
+
+function error($msg, $code=400) {
+  if($code == 400)
+    header("HTTP/1.1 400 Bad Request");
+  header("Content-type: text/plain");
+  echo $msg."\n";
+  die();
+}
+
+if(!isset($_POST['source']) || !isset($_POST['target'])) {
+  if(strtolower($_SERVER['REQUEST_METHOD']) == 'post') {
+    error("Missing source or target properties");
+  } else {
+  ?>
+    <link rel="stylesheet" type="text/css" href="/semantic/semantic.min.css">
+<div class="ui container">
+  <h2>Send a Webmention</h2>
+  <form action="/webmention.php" method="post">
+    Source: <input type="url" name="source" value="" style="width:400px;"><br><br>
+    Target: <input type="url" name="target" value="<?= $targetBaseURL ?>/" style="width:400px;"><br><br>
+    <input type="submit" value="Send Webmention">
+  </form>
+</div>
+  <?php
+  die();
+  }
+}
+
+$sourceURL = $_POST['source'];
+$targetURL = $_POST['target'];
+
+$targetURLParts = parse_url($targetURL);
+if(empty($targetURLParts['path']))
+  $targetURLParts['path'] = '/';
+
+$events = get_active_events();
+
+$event = false;
+foreach($events as $e=>$tito) {
+  $path = '/'.$e;
+  if($path == $targetURLParts['path']) {
+    $event = $e;
+  }
+}
+
+if(!$event) {
+  error("Webmentions are only accepted for current events. The target in your request was not the URL of a current event.");
+}
+
+$response = $http->get($xrayBaseURL.'/parse?url='.urlencode($sourceURL).'&target='.urlencode($targetURL));
+$data = json_decode($response['body'], true);
+
+// XRay tells us if the URL didn't link to the target
+if(isset($data['error'])) {
+  error($data['error_description']);
+}
+
+$source = $data['data'];
+
+if(!is_array($source)) {
+  error("There was a problem parsing the source URL");
+}
+
+// Check the source for in-reply-to and rsvp properties
+if(!array_key_exists('in-reply-to', $source)) {
+  error("Your post doesn't seem to have an in-reply-to property");
+}
+
+if(!in_array($targetURL, $source['in-reply-to'])) {
+  error("It looks like your post does not have the event URL in the in-reply-to property");
+}
+
+if(!array_key_exists('rsvp', $source)) {
+  error("Your post doesn't have an 'rsvp' property");
+}
+
+if(!file_exists(dirname(__FILE__).'/../data/'.$event)) {
+  mkdir(dirname(__FILE__).'/../data/'.$event, 0755);
+}
+
+// Store the response data to disk so that it's rendered on the event page
+$folder = dirname(__FILE__).'/../data/'.$event.'/'.md5($sourceURL);
+
+if(strtolower($source['rsvp']) == 'yes') {
+
+  @mkdir($folder);
+
+  if($source['author']['photo']) {
+    $tmp = tempnam(sys_get_temp_dir(), 'img');
+    $img = $http->get($source['author']['photo']);
+    if($img['body']) {
+      file_put_contents($tmp, $img['body']);
+      if(preg_match('/^<\?xml.+<svg.+<\/svg>$/', trim($img['body']))) {
+        $ext = 'svg';
+      } else {
+        $type = exif_imagetype($tmp);
+        $ext = false;
+        switch($type) {
+          case IMAGETYPE_GIF:
+            $ext = 'gif'; break;
+          case IMAGETYPE_JPEG:
+            $ext = 'jpg'; break;
+          case IMAGETYPE_PNG:
+            $ext = 'png'; break;
+          case IMAGETYPE_ICO:
+            $ext = 'ico'; break;
+        }
+      }
+      if($ext) {
+        copy($tmp, $folder . '/photo.'.$ext);
+      }
+    }
+  }
+
+  $filename = $folder . '/post.json';
+  $data = [
+    'received' => date('c'),
+    'source' => $sourceURL,
+    'data' => $source
+  ];
+  file_put_contents($filename, json_encode($data));
+
+  echo "Thanks! Your RSVP is listed on the event page now!\n";
+} else {
+
+  if(file_exists($folder . '/post.json')) {
+    unlink($folder . '/post.json');
+  }
+
+  echo "Thanks! Your RSVP was received, but you won't be listed on the event page because your RSVP was not \"yes\"\n";
+}
