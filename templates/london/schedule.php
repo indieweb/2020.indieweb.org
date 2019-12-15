c7a3db1c5d37075fa83e893b94bf66d33012a5a6
Cheukting <cheukting.ho@gmail.com>
2019-12-15 15:28:10 +0000

    Adding London event

---
+++ templates/london/schedule.php
@@ -0,0 +1,71 @@
+<p>
+  <i class="calendar icon"></i> <time class="dt-start" datetime="2020-03-14">March 14</time>-<time class="dt-end" datetime="2020-03-15">15</time>, 2020<br>
+  <i class="marker icon"></i> <span class="h-card p-location"><span class="p-name">The Microsoft Reactor London</span>, <span class="p-street-address">70 Wilson Street</span>, <span class="p-locality">London</span>, <span class="p-region">UK</span></span>
+</p>
+
+<div class="schedule-grid">
+<!--  <div class="day">-->
+<!--    <div class="header gold-bkg">-->
+<!--        Friday, February 22nd<br/>-->
+<!--        <span class="featured">Pre-Camp Meetup</span>-->
+<!--    </div>-->
+<!--    <div class="summary">-->
+<!--        The Pre-Camp Meetup is an informal social meetup for IndieWebCamp participants to catch up and get to know each other before the camp. -->
+<!--    </div>-->
+<!--    <div class="details">-->
+<!--        <ul>-->
+<!--            <li><span class="time">19:00</span> <span class="featured">Pre-Camp Meetup!</span></li>-->
+<!--        </ul>-->
+<!--        <span><a href="http://joesquared.com/">Joe Squared</a><br/><span>33 W. North Ave.</span></span>-->
+<!--        <p style="font-size:0.8em; margin-top: 1em;">Joe Squared is in Baltimore's Station North with great pizza, risotto, drink options, and more. Vegan and GF friendly.</p>-->
+<!--        <ul>-->
+<!--            <li><span class="time">21:00</span> <span class="featured">IndieWeb-themed* Comedy Show</span></li>-->
+<!--        </ul>-->
+<!--        <span><a href="http://www.bigimprov.org/">Baltimore Improv Group</a><br/><span>1727 N. Charles St.</span></span>-->
+<!--        <p style="font-size:0.8em; margin-top: 1em;">The Baltimore Improv Group (BIG) is a non-profit for improvisational theatre shows and instruction. Co-organizer Marty will be appearing in the evening's 9PM show.</p>-->
+<!--    </div>-->
+<!--  </div>-->
+  <div class="day">
+    <div class="header orange-bkg">
+      Saturday, March 14<br>
+      <span class="featured">Learn, Share, Discuss!</span>
+    </div>
+    <div class="summary">
+      Day 1 starts with introductions, demos of personal sites, a review of the IndieWeb community and development building blocks, then BarCamp-style brainstorming and discussion sessions.
+    </div>
+    <div class="details">
+      <ul>
+       <li><span class="time">9:00</span> Doors open, coffee, and breakfast!</li>
+       <li><span class="time">10:00</span> Welcome</li>
+       <li><span class="time">10:15</span> Keynote: TBC</li>
+       <li><span class="time">10:45</span> Personal site demos</li>
+       <li><span class="time">11:30</span> <a href="https://indieweb.org/session_facilitating">Session proposals &amp; scheduling</a></li>
+       <li><span class="time">12:00</span> Group photo &amp; lunch</li>
+       <li><span class="time">13:00</span> Sessions (45min)</li>
+       <li><span class="time">16:45</span> Day 2 preview</li>
+      </ul>
+    </div>
+  </div>
+  <div class="day">
+    <div class="header red-bkg">
+      Sunday, March 15<br>
+      <span class="featured">Create, Hack, Demo!</span>
+    </div>
+    <div class="summary">
+      Day 2 make things on and for your personal site! Collaborate with others or work on your own to improve at least one thing on your personal site, no matter how small or how epic.
+    </div>
+    <div class="details">
+      <ul>
+       <li><span class="time">9:00</span> Doors open, coffee, and breakfast!</li>
+       <li><span class="time">10:00</span> Day 2 kickoff, session planning</li>
+       <li><span class="time">10:30</span> Creating sessions</li>
+       <li><span class="time">12:00</span> Group photo &amp; lunch</li>
+       <li><span class="time">13:00</span> Sessions continue</li>
+       <li><span class="time">16:00</span> Demos</li>
+       <li><span class="time">17:00</span> Camp closed!</li>
+      </ul>
+    </div>
+  </div>
+</div>
+
+<br>
---
+++ templates/london/sponsors.php
@@ -0,0 +1,33 @@
+<!--<p>Special thanks to our sponsors who make it possible to provide food and space for the IndieWebCamp</p>-->
+
+<ul>
+<!--  <li>
+    <span class="logo">
+      <a href="https://www.digitalharbor.org/"><img src="https://indieweb.org/sponsor-logos/full/digital-harbor-foundation-logo.png" style="width: 130px;"></a>
+    </span>
+    <span class="description"><a href="https://www.digitalharbor.org/">The Digital Harbor Foundation</a> is dedicated to fostering learning, creativity, productivity, and community through education. In 2013 we transformed a closed-down rec center in Baltimore City into a vibrant Tech Center for youth.</span>
+  </li>-->
+  <li>
+    <span class="logo">
+      <a href="https://terminusdb.com/"><img src="https://terminusdb.com/img/logos/logo.svg" style="width: 90px;"></a>
+    </span>
+    <span class="description"><a href="https://terminusdb.com/">TerminusDB</a> is a database built for data people. Terminus is a model driven graph database designed specifically for the web-age.</span>
+  </li>
+  <li>
+    <span class="logo">
+      <a href="https://developer.microsoft.com/en-gb/reactor/"><img src="https://bindersmedia-visuals.azureedge.net/images-production//AWhW/zmLA/pxPvH0YhBIlh/img-aabcf31c-0bcd-46f2-8684-3388292738d2/MEDIUM?st=2019-12-15T13%3A36%3A48Z&se=2019-12-15T16%3A56%3A48Z&sp=r&sv=2018-03-28&sr=b&sig=Lz2vMhEQjCL%2BQQt5G1SHf1aFl0kUW9ngXsJNMcDCxSo%3D" style="width: 90px;"></a>
+    </span>
+    <span class="description">In the heart of London's tech zone, <a href="https://developer.microsoft.com/en-gb/reactor/Location/London">the London Reactor</a> is a hub for all things developer and startup. 70 Wilson Street boasts an impressive and flexible event space and is home to the Microsoft for Startups program.</span>
+  </li>
+  <li>
+    <span class="logo">
+      <a href="http://stickermule.com/supports/indieweb20-sponsorship"><img src="https://indieweb.org/images/b/b1/stickermule-logo.png" style="width: 90px;"></a>
+    </span>
+    <span class="description"><a href="http://stickermule.com/supports/indieweb20-sponsorship">Sticker Mule</a> is the fastest and easiest way to buy custom printed products.</span>
+  </li>
+</ul>
+
+
+<h4 class="ui header">How to Sponsor</h4>
+<p>We would be thrilled to list you as a sponsor! See our sponsorship prospectus for details, or <a href="https://opencollective.com/indieweb">become a monthly supporter</a>.</p>
+<a class="ui button" href="https://indiewebcamp.com/images/2/2d/indiewebcamp-sponsorship-prospectus.pdf">Download Sponsorship Prospectus (PDF)</a>
