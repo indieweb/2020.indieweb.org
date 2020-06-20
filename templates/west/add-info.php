d221f78aa8fac278356f9624cbd293c27b0b24a9
Tantek Çelik <blog@tantek.com>
2020-06-19 21:57:26 -0700

    remove travel fund info

--- templates/west/add-info.php
+++ templates/west/add-info.php
@@ -1,8 +1,2 @@
-<!--
-<div style="border: 1px ##f4b058 solid; background: #ffd9a8; border-radius: 8px; padding: 12px; margin-bottom: 2em;">
-  Travel assistance applications are now closed, and all applicants have been notified. Thanks to everyone who submitted an application! Please see our <a href="https://indieweb.org/2019#Remote_Participation">remote particiation</a> options if you are not able to attend in person.
-</div>
--->
-
 <p>For addtional information please visit 
   the <a href="https://indieweb.org/2020/West" target="_blank">IndieWebCamp West wiki page</a>.</p>
