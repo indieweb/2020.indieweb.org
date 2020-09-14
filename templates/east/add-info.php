40feead97749686e47561f52ad249872d0ca7061
David Shanske <david@shanske.com>
2020-09-14 19:07:51 -0400

    East 2020 Event

---
+++ templates/east/add-info.php
@@ -0,0 +1,2 @@
+<p>For addtional information please visit 
+  the <a href="https://indieweb.org/2020/East" target="_blank">IndieWebCamp East wiki page</a>.</p>
---
+++ templates/east/nav.php
@@ -0,0 +1,6 @@
+    <a class="item" href="#about">About</a>
+    <a class="item" href="#register">Register</a>
+    <a class="item" href="#schedule">Schedule</a>
+    <a class="item" href="#location">Location</a>
+    <a class="item" href="#add-info">Additional Information</a>
+    <a class="item" href="#sponsors">Sponsors</a>
